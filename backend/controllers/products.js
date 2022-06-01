f67f237081e2b5c0faee9e287b90746726772c93
AhmadAlafaghani <afaghani.ahmed@gmail.com>
2022-06-01 18:39:25 +0300

    ay7aga

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,5 +1,5 @@
 //-------------------------------------------------- shaker
-
+const connection = require("../models/db");
 
 
 
@@ -68,9 +68,26 @@
 
 
 // ------------------------------------------------------------ Afgani
+const getAllProduct = (req, res) => {
+    const query = `SELECT * FROM products WHERE is_deleted=0;`;
+    connection.query(query, (err, result) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          massage: "server error",
+          err: err,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        massage: "All the product",
+        result: result,
+      });
+    });
+  };
 
 
-
+module.exports = {getAllProduct}
 
 
 
--- backend/routes/prodcuts.js
+++ backend/routes/prodcuts.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
-// const {  } = require("../controllers/products");
+const { getAllProduct } = require("../controllers/products");
 
 const productRouter = express.Router();
 
-// productRouter.post("/");
+productRouter.get("/",getAllProduct);
 
 module.exports = productRouter;
