facf9ed6343593e070056bf0709e7f599c827a4c
shakirabugarbia <shakir.abugarbia@gmail.com>
2022-06-01 16:51:56 +0300

    create category

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,195 +1,33 @@
 // ----------------------------------------------- shaker
 const connection = require("../models/db");
 
+const createCategory = (req, res) => {
+  const { category_title, category_img } = req.body;
+  const query = `INSERT INTO foodCategories (category_title, category_img) VALUES (?,?);`;
+  const data = [category_title, category_img];
 
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "Server error",
+        err: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      massage: "category created",
+      result: result,
+    });
+  });
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = {
+  createCategory,
+};
 
 //------------------------------------------------------ marah
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //-----------------------------------------------afgani
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //-------------------------------------------------------fouad
