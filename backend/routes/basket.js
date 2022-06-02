3614787b7692a04a873012ea86f8e6f3a4c60ecd
AhmadAlafaghani <afaghani.ahmed@gmail.com>
2022-06-02 20:46:50 +0300

    btata

--- backend/routes/basket.js
+++ backend/routes/basket.js
@@ -5,12 +5,14 @@ const {
   addAndUpdateToCart,
   viewCart,
   removefromcart,
+  removeAndDecreas,
 } = require("../controllers/basket");
 
 const basketRouter = express.Router();
 
 basketRouter.post("/:product_id", authentication, addAndUpdateToCart);
 basketRouter.get("/", authentication, viewCart);
-basketRouter.put("/:product_id",authentication, removefromcart);
+basketRouter.put("/:product_id", authentication, removefromcart);
+basketRouter.put("/basket/:product_id", authentication, removeAndDecreas);
 
 module.exports = basketRouter;
