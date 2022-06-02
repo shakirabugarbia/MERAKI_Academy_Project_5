cc9af92b04d8b25a8587519944e3b0a325bfb4d8
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-06-02 13:19:47 +0300

    auth

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -6,7 +6,7 @@ const authorization = (string) => {
     const data = [user_id];
     const query = `SELECT * FROM users U WHERE U.id = (?)`;
     connection.query(query, data, (err, result) => {
-      const query = `SELECT * FROM role_permission RP INNER JOIN permissions P ON RP.permission_id = P.id WHERE RP.role_id = (?) AND P.permission = (?)`;
+      const query = `SELECT * FROM roles_permissions RP INNER JOIN permissions P ON RP.permission_id = P.id WHERE RP.role_id = (?) AND P.permission = (?)`;
       const data = [result[0].role_id, string];
       connection.query(query, data, (err, result) => {
         if (result.length) {
