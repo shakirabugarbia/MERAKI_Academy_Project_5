045f94f10363d47fe8e089cfbe626e724aaa9211
shakirabugarbia <shakir.abugarbia@gmail.com>
2022-06-03 16:45:49 +0300

    team

--- frontend/src/component/login/index.js
+++ frontend/src/component/login/index.js
@@ -4,86 +4,76 @@ import "./style.css";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 
-import { login} from "../../redux/reducers/auth";
+import { login } from "../../redux/reducers/auth";
 
 
 export const Login = () => {
+  const navigate = useNavigate();
 
   const dispatch = useDispatch();
-  const {token,isLoggedIn} = useSelector((state) => {
+  const { token, isLoggedIn } = useSelector((state) => {
     return {
       token: state.auth.token,
       isLoggedIn: state.auth.isLoggedIn,
     };
   });
 
-// ===================================
+  // ===================================
 
-  const history = useNavigate();
+
 
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [password, setPassword] = useState("");
   //===================================
 
-  const loginn =()=> {
-
- 
-    axios.post("http://localhost:5000/login", {
+  const loginn = () => {
+    axios
+      .post("http://localhost:5000/login", {
         email,
-        password
+        password,
       })
-    .then((result)=>{
-if(result.data.success){
-    setMessage('login succefull')
-    dispatch(login(result.data.token));
-} 
- 
-    })
-      
-   .catch ((err)=> {
-     
-      setMessage("Error happened while Login, please try again");
-    })
-}
+      .then((result) => {
+        if (result.data.success) {
+          setMessage("login succefull");
+          dispatch(login(result.data.token));
+        }
+      })
+
+      .catch((err) => {
+        setMessage("Error happened while Login, please try again");
+      });
+  };
 
   //===============================================================
-
-
 
   return (
     <>
       <div className="Form">
-      
-      
-          <br />
+        <br />
 
-          <input
-            type="email"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <br />
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <br />
-          <button
-            onClick={() => {
-              loginn();
-            }}
-          >
-            Login
-          </button>
-          <p>{message}</p>
-      
-
-       
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button
+          onClick={() => {
+            loginn();
+            navigate("/");
+          }}
+        >
+          Login
+        </button>
+        <p>{message}</p>
       </div>
     </>
   );
 };
-
-
--- frontend/src/component/navbar/index.js
+++ frontend/src/component/navbar/index.js
@@ -1,14 +1,15 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import "./style.css"
+import "./style.css";
 import { useDispatch, useSelector } from "react-redux";
-import {  logout } from "../../redux/reducers/auth";
-
+import { logout } from "../../redux/reducers/auth";
+import { useNavigate } from "react-router-dom";
 //===============================================================
 
 const NavBar = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {token,isLoggedIn} = useSelector((state) => {
+  const { token, isLoggedIn } = useSelector((state) => {
     // specify which state to subscribe to (state tree => reducer => state name )
     return {
       token: state.auth.token,
@@ -18,15 +19,65 @@ const NavBar = () => {
   //===============================================================
 
   return (
-    
-      <div className="NavBar">
-     
-            <Link className="Link" to="/">
-              Register
-            </Link>
-            <Link to="/login">Login</Link>
-            </div>
-         )
+    <div className="NavBar">
+      {isLoggedIn ? (
+        <>
+          <Link className="Link" to="/">
+            Home
+          </Link>
+          <Link className="Link" to="/register">
+            Register
+          </Link>
+          <button
+            className="logout"
+            onClick={() => {
+              dispatch(logout());
+              navigate("/");
+            }}
+          >
+            Logout
+          </button>
+        </>
+      ) : (
+        <>
+          <Link className="Link" to="/">
+            Home
+          </Link>
+          <Link className="Link" to="/register">
+            Register
+          </Link>
+          <Link to="/login">Login</Link>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default NavBar;
+
+// {state.isloggedIn ? (
+//   <>
+//     <Link className="Link" to="/tasks">
+//       Tasks
+//     </Link>
+//     <Link className="Link" to="/complete tasks">
+//       copmlete task
+//     </Link>
+//     <button
+//       className="logout"
+//       onClick={() => {
+//         dispatch(logout());
+//         navigate("/");
+//       }}
+//     >
+//       Logout
+//     </button>
+//   </>
+// ) : (
+//   <>
+//     <Link className="Link" to="/">
+//       Register
+//     </Link>
+//     <Link to="/login">Login</Link>
+//   </>
+// )}
