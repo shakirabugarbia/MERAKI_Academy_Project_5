dab19b63243997311f45d42d4c50038f2c9695d2
AhmadAlafaghani <afaghani.ahmed@gmail.com>
2022-06-08 14:52:28 +0300

    adminside

--- frontend/src/component/ProdusctsAdmin/index.js
+++ frontend/src/component/ProdusctsAdmin/index.js
@@ -1,8 +1,159 @@
-const ProductsAdminSide = ()=>{
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import {
+  addProductts,
+  deleteproductts,
+  updateproductts,
+  setProducts,
+  setProductName,
+} from "../../redux/reducers/products/index";
+import {
+  addCategoriess,
+  deleteCategoriess,
+  updateCategoriess,
+  setCategories,
+} from "../../redux/reducers/categories";
+import {
+  addTypeOfFood,
+  deleteTypeOfFood,
+  updateTypeOfFood,
+  setTypeOfFood,
+} from "../../redux/reducers/typeoffoods/index";
 
-   return(
-       <div><h1>Products Admin Side</h1></div>
-   )
-}
+const ProductsAdminSide = () => {
+  const dispatch = useDispatch();
+
+  const { token, isLoggedIn } = useSelector((state) => {
+    return {
+      token: state.auth.token,
+      isLoggedIn: state.auth.isLoggedIn,
+    };
+  });
+
+  const productsState = useSelector((state) => {
+    return {
+      products: state.products.products,
+    };
+  });
+
+  const typesOffoodsState = useSelector((state) => {
+    return {
+      typesOffoods: state.typeoffood,
+    };
+  });
+
+  const { categories, category_id } = useSelector((state) => {
+    return {
+      categories: state.categories.categories,
+      category_id: state.categories.category_id,
+    };
+  });
+
+  const [show, setShow] = useState(false);
+  const [productName, setProductName] = useState("");
+  const [description, setDescription] = useState("");
+  const [img, setImg] = useState("");
+  const [price, setPrice] = useState("");
+  const [updateBox, setUpdateBox] = useState(false);
+  const [productId, setProductId] = useState(false);
+  const [message, setMessage] = useState("");
+  const [userId, setUserId] = useState("");
+  const [hide, setHide] = useState(false);
+
+  const gatAllproducts = () => {
+    axios
+      .get(`http://localhost:5000/product`)
+      .then((result) => {
+        dispatch(setProducts(result.data.result.result));
+        setMessage(result.data.message);
+      })
+      .catch((err) => {
+        setShow(true);
+        setMessage(err.message);
+      });
+  };
+  const gatAllCategories = () => {
+    axios
+      .get(`http://localhost:5000/category/allCategories`)
+      .then((result) => {
+        dispatch(setCategories(result.data.result));
+        setMessage(result.data.message);
+        setShow(true);
+      })
+      .catch((err) => {
+        setMessage(err.message);
+      });
+  };
+
+  const getAllTypeOfFood = () => {
+    axios
+      .get(`http://localhost:5000/typeOfFood`)
+      .then((result) => {
+        dispatch(setTypeOfFood(result.data.result));
+        setShow(true);
+      })
+      .catch((err) => {
+        setMessage(err.message);
+      });
+  };
+  // const createProduct = ()=>{
+  //   axios.post(`http://localhost:5000/product/${}`,{}).then({}).catch({})
+  // }
+  const updateproducttss = async (id) => {
+    try {
+      await axios.put(`http://localhost:5000/product/update/${id}`, {
+        productName,
+        img,
+        description,
+        price,
+      });
+      dispatch(
+        updateproductts({
+          productName: productName,
+          img: img,
+          description: description,
+          price: price,
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    gatAllCategories();
+    gatAllproducts();
+    getAllTypeOfFood();
+  }, []);
+
+  return (
+    <div className="main">
+      <h1>Products Admin Side</h1>
+      <div className="Products-Container">
+        {show &&
+          productsState.products.map((element, index) => {
+            return (
+              <div className="Product-Container" key={index}>
+                <div className="product-All-Detiles">
+                  <div className="Product-Img-Dev">
+                    <img className="imgg" src={element.img} />
+                  </div>
+                  <div className="pppp">
+                    <div className="t">
+                      <div className="pName"> {element.productName}</div>
+                      <div className="desc"> {element.description}</div>
+                      <div className="price">{element.price}JD</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default ProductsAdminSide;
