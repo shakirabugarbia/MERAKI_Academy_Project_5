330ef05c92c7e44859d0ee18794fa8106a5ce398
shakirabugarbia <shakir.abugarbia@gmail.com>
2022-06-08 23:48:17 +0300

    fix

---
+++ frontend/src/redux/reducers/order/index.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const order = createSlice({
+  name: "order",
+  initialState: {
+    order: [],
+    items: [],
+  },
+  reducers: {
+    addOrder: (state, action) => {
+      state.order.push(action.payload);
+    },
+    setItems: (state, action) => {
+      state.items.push(action.payload);
+    },
+  },
+});
+
+export const { addOrder ,setItems} = order.actions;
+export default order.reducer;
