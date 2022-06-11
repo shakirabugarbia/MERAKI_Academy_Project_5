dcabcf0bf30727478802dac25bf6887a50b9a810
shakirabugarbia <shakir.abugarbia@gmail.com>
2022-06-11 21:32:03 +0300

    login first

--- frontend/src/component/AdminPanale/index.js
+++ frontend/src/component/AdminPanale/index.js
@@ -1,32 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "./style.css";
-import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
+
+import { useDispatch, useSelector } from "react-redux";
 
 const Admin = () => {
+  const navigate = useNavigate();
+  const { token, isLoggedIn } = useSelector((state) => {
+    return {
+      token: state.auth.token,
+      isLoggedIn: state.auth.isLoggedIn,
+    };
+  });
   return (
     <div className="main">
-      <div></div>
-      <table className="tabelss">
-        <tr>
-          <td>
-            <Link className="linkss" to={"/UserAdminPanel"}>User Section</Link>
-          </td>{" "}
-          <td>
-            <Link className="linkss" to={"/ProductAdminPanel"}>Product Section</Link>
-          </td>
-        </tr>{" "}
-        <tr>
-          <td>
-            <Link className="linkss" to={"/"}>Category Section</Link>
-          </td>
-          <td>
-            {" "}
-            <Link className="linkss" to={"/"}>Foods type Section</Link>
-          </td>
-        </tr>
-      </table>
+      {isLoggedIn ? (
+        <table className="tabelss">
+          <tr>
+            <td>
+              <Link className="linkss" to={"/UserAdminPanel"}>
+                User Section
+              </Link>
+            </td>{" "}
+            <td>
+              <Link className="linkss" to={"/ProductAdminPanel"}>
+                Product Section
+              </Link>
+            </td>
+          </tr>{" "}
+          <tr>
+            <td>
+              <Link className="linkss" to={"/"}>
+                Category Section
+              </Link>
+            </td>
+            <td>
+              {" "}
+              <Link className="linkss" to={"/"}>
+                Foods type Section
+              </Link>
+            </td>
+          </tr>
+        </table>
+      ) : (
+        <div>
+          Login first
+          <button
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            go to login
+          </button>
+        </div>
+      )}
     </div>
   );
 };
