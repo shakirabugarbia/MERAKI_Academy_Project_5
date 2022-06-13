9b0f893f1a57f6779e93d01a651e96d0ff5c22df
shakirabugarbia <shakir.abugarbia@gmail.com>
2022-06-13 20:09:17 +0300

    we got our data base again

--- backend/models/db.js
+++ backend/models/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const  connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
  });
  
