f3df24d781ff0d4aa1da889af8e9198075e6826d
shakirabugarbia <shakir.abugarbia@gmail.com>
2022-06-15 12:47:53 +0300

    basket counter

--- frontend/src/component/navbar/index.js
+++ frontend/src/component/navbar/index.js
@@ -6,7 +6,7 @@ import { logout } from "../../redux/reducers/auth";
 import { useNavigate } from "react-router-dom";
 import { AiOutlineUser, AiOutlineUserAdd } from "react-icons/ai";
 
-import { FaBeer } from 'react-icons/fa';
+import { FaBeer } from "react-icons/fa";
 
 import { GrLogout } from "react-icons/gr";
 
@@ -20,6 +20,13 @@ const NavBar = () => {
     return {
       token: state.auth.token,
       isLoggedIn: state.auth.isLoggedIn,
+    };
+  });
+  const basketState = useSelector((state) => {
+    return {
+      basket: state.basket.basket,
+      amount: state.basket.amount,
+      price: state.basket.price,
     };
   });
 
@@ -53,8 +60,7 @@ const NavBar = () => {
                 localStorage.setItem("view", true);
               }}
             >
-              Basket 
-         
+              Basket | {basketState.amount}
             </Link>
             <a
               className="Link"
