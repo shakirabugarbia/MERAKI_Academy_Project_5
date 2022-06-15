f956cf22722cfb63ca0fc8183a04a106f1c2e035
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-06-15 15:58:33 +0300

    Editing Footer

--- frontend/src/component/Footer/index.js
+++ frontend/src/component/Footer/index.js
@@ -5,7 +5,7 @@ import {
   AiFillFacebook,
   AiFillTwitterCircle,
   AiFillInstagram,
-  AiFillApple
+  AiFillApple,
 } from "react-icons/ai";
 import { FaGooglePlay } from "react-icons/fa";
 import "./style.css";
@@ -13,48 +13,72 @@ import "./style.css";
 export const Footer = () => {
   return (
     <div className="footer">
-      
-        <div className="footer-content">
-          
-          <div className="sub">
+      <div className="footer-content">
+        <div className="sub">
+          <div>
+            <b>Company</b>
+            <br />
+            <Link className="suggestions" to={"/contactUS"}>
+              About
+            </Link>
+            <br />
+            <Link className="suggestions" to={"/contactUS"}>
+              Blog
+            </Link>
+            <br/>
+            <Link className="suggestions" to={"/contactUS"}>
+              Suggestion
+            </Link>
+          </div>
+          <div>
+            <b>For Foodies</b>
+            <p>Code of conduct</p>
+            <p>Community</p>
+          </div>
+          <div className="For_restaurant">
+            <b> For Restaurant</b>
+            <br />
+            <Link className="suggestions" to={"/"}>
+              Restauran
+            </Link>
+            <br />
+            <Link className="suggestions" to={"/"}>
+              Business
+            </Link>
+            <br />
+            <Link className="suggestions" to={"/location"}>
+              Our Location
+            </Link>
+          </div>
+          <div>
+            <b>For You</b>
+            <br />
+            <Link className="suggestions" to={"/"}>
+              Privacy
+            </Link>
+            <br />
+            <Link className="suggestions" to={"/"}>
+              Security
+            </Link>
+            <br />
+            <Link className="suggestions" to={"/"}>
+              Terms
+            </Link>
+          </div>
+          <div>
+            <b>Social links</b>
             <div>
-              <b>Company</b>
-              <p>About</p>
-              <p>Blog</p>
-              <Link className="suggestions" to={"/contactUS"}>suggestion</Link>
+              <AiFillFacebook />
+              <AiFillTwitterCircle />
+              <AiFillInstagram />
             </div>
             <div>
-              <b>For Foodies</b>
-              <p>Code of conduct</p>
-              <p>Community</p>
-            </div>
-            <div className="For_restaurant">
-              <b>For Restaurant</b>
-              <p>Restaurant</p>
-              <p>Business</p>
-              <Link className="suggestions"  to={"/location"}>Location</Link>
-            </div>
-            <div>
-              <b>For You</b>
-              <p>Privacy</p>
-              <p>Security</p>
-              <p>Terms</p>
-            </div>
-            <div>
-              <b>Social links</b>
-              <div>
-                <AiFillFacebook />
-                <AiFillTwitterCircle />
-                <AiFillInstagram />
-              </div>
-              <div>
-                <AiFillApple />
-                <FaGooglePlay />
-              </div>
+              <AiFillApple />
+              <FaGooglePlay />
             </div>
           </div>
         </div>
-      
+      </div>
     </div>
   );
 };
