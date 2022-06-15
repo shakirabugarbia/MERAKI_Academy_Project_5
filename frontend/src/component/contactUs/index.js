353a978d42e4f0579cca3a7ed6f626c8dd30933e
MarahShihadeh <shihadehmarah@gmail.com>
2022-06-15 18:45:13 +0300

    conact_us_css

--- frontend/src/component/contactUs/index.js
+++ frontend/src/component/contactUs/index.js
@@ -1,6 +1,10 @@
-import "./style.css";
 import React, { useRef } from "react";
+import "./style.css";
 import emailjs from "@emailjs/browser";
+import { IoLocationOutline } from "react-icons/io5";
+import { AiOutlineMail } from "react-icons/ai";
+import { MdPhoneAndroid } from "react-icons/md";
+
 
 const Contact = () => {
   const form = useRef();
@@ -24,30 +28,76 @@ const Contact = () => {
         }
       );
   };
-
   return (
     <div class="container">
-      <div class="contact-form">
-        <form ref={form} onSubmit={sendEmail}>
-          <h3 class="title">Contact us</h3>
-          <div class="input-container">
-            <input type="text" name="name" class="input" />
-            <label for="">Username</label>
-            <span>Username</span>
+      <div class="form">
+        <div class="contact-info">
+          <h3 class="title">Let's get in touch</h3>
+          <p class="text">BigBite</p>
+
+          <div class="info">
+            <div class="information">
+              <IoLocationOutline /> <p> Jordan-Amman / Al-Jubiyha </p>
+            </div>
+            <div class="information">
+              <AiOutlineMail /> <p>BigBite@JoRes.com</p>
+            </div>
+            <div class="information">
+              <MdPhoneAndroid /> <p>053-8896-4247</p>
+            </div>
           </div>
 
-          <div class="input-container">
-            <input type="tel" name="phone" class="input" />
-            <label for="">Phone</label>
-            <span>Phone</span>
+          <div>
+            <p>Connect with us :</p>
+            <div class="social-icons">
+              <a href="#">
+                <i class="fa-brands fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </div>
           </div>
-          <div class="input-container textarea">
-            <textarea name="message" class="input"></textarea>
+        </div>
 
-            <span>Message</span>
-          </div>
-          <input type="submit" value="Send" class="btn" />
-        </form>
+        <div class="contact-form">
+          <form ref={form} onSubmit={sendEmail}>
+            <h3 class="title">Contact us</h3>
+            <div class="input-container">
+              <input
+                type="text"
+                name="name"
+                placeholder="Username"
+                class="input"
+              />
+              {/* <label for="">Username</label> */}
+              <span>Username</span>
+            </div>
+
+            <div class="input-container">
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Phone"
+                class="input"
+              />
+              {/* <label for="">Phone</label> */}
+              <span>Phone</span>
+            </div>
+            <div class="input-container textarea">
+              <textarea name="message" class="input"></textarea>
+
+              <span>Message</span>
+            </div>
+            <input type="submit" value="Send" class="btn" />
+          </form>
+        </div>
       </div>
     </div>
   );
